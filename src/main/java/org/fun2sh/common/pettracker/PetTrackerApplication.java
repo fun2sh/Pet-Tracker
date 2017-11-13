81318676ea12775d4e2b3f9a38ac592f2ea7bdaa
Suresh Kumar <suresh.kumar@colorado.edu>
2017-11-13 10:49:47 -0700

    Spring Boot Initialization

---
+++ src/main/java/org/fun2sh/common/pettracker/PetTrackerApplication.java
@@ -0,0 +1,12 @@
+package org.fun2sh.common.pettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTrackerApplication.class, args);
+	}
+}
